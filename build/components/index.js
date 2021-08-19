1f6453252934133a481c2e2479e438460be76726
Komron Bakozoda <kbakozoda@twilio.com>
2021-08-20 01:55:50 +0200

    remove build from tracking

--- build/components/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AuthComponent = require("./Auth");
-exports.AuthComponent = AuthComponent;
-const UserComponent = require("./User");
-exports.UserComponent = UserComponent;
-const CookComponent = require("./Cook");
-exports.CookComponent = CookComponent;
-//# sourceMappingURL=index.js.map
